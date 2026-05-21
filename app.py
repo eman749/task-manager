7c3752f065b5cd31608b870c2c635a56d2560fa8
eman749 <etamer749@gmail.com>
2026-05-21 10:27:45 +0300

    load environment variables from .env

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'dev-only-change-in-production')
